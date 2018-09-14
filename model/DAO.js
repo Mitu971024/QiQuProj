3706fe29dfc8fc9861fb18f129c66bfe24c09126
mitu <1206061634@qq.com>
2018-09-14 11:12:30 +0800

    修改项目基础配置

---
+++ model/DAO.js
@@ -0,0 +1,23 @@
+var mysql = require('mysql');
+var dbconfig = require('../model/dbconfig');
+
+function DAO(sql,sqlparams,callback) {
+    var connection = mysql.createConnection(dbconfig.options)
+    connection.connect(function (err) {
+        if (err){
+            console.log('数据库连接失败！' + err.message);
+            callback(null);
+            return;
+        }
+        connection.query(sql,sqlparams,function (e,data) {
+            if (err){
+                console.log('数据库连接失败！' + e.message);
+                callback(null);
+                return;
+            }
+            callback(data);
+            connection.end();
+        })
+    })
+}
+module.exports.DB = DAO;
---
+++ model/dbconfig.js
@@ -0,0 +1,8 @@
+var options = {
+    host:'127.0.0.1',
+    port:3306,
+    user:'root',
+    password:'root',
+    database:'compangdb'
+};
+module.exports.options = options;
