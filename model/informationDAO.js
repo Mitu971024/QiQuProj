990696c9abad5d8882d1adac6710d34a304b83fd
miaomiao <1299932158@qq.com>
2018-10-21 18:21:27 +0800

    10.21日提交

--- model/informationDAO.js
+++ model/informationDAO.js
@@ -1,34 +1,21 @@
 var DAO=require('../model/DAO');
 
 class DB{
-    //获取全部热门数据
-    // getInformation(type){
-    //     return DAO('SELECT zTitle,zImg,zContent,zTime,zFrom,zCheck FROM `information` ' +
-    //         'where zType=? order by zTime desc,zCheck desc',[type]);
-    // }
 
      // 获取全部热门数据
         gethotInfo(){
-            // return DAO('SELECT zTitle,zImg,zContent,zTime,zFrom,zCheck,zType FROM `information` ' +
-            //     'where zType="热门资讯" order by zTime desc,zCheck desc',[]);
             return DAO('call gethotinformation;',[])
         }
     //获取今日资讯
     gettodayInfo(){
-        // return DAO('SELECT zTitle,zImg,zContent,zTime,zFrom,zCheck,zType FROM `information` ' +
-        //     'where zType="今日资讯" order by zTime desc,zCheck desc',[]);
         return DAO('call gettodayInfo;',[])
     }
     //获取骑闻数据
     getnewsInfo(){
-        // return DAO('SELECT zTitle,zImg,zContent,zTime,zFrom,zCheck,zType FROM `information` ' +
-        //     'where zType="骑闻" order by zTime desc,zCheck desc',[]);
         return DAO('call getnewsInfo;',[])
     }
     //获取其他数据
     getotherInfo(){
-        // return DAO('SELECT zTitle,zImg,zContent,zTime,zFrom,zCheck,zType FROM `information` ' +
-        //     'where zType="其它" order by zTime desc,zCheck desc',[]);
         return DAO('call getotherInfo;',[])
     }
     //获取详情内容和更新点击量
